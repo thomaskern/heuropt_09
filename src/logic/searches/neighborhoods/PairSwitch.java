28cd6a6f680a5207716aec852c9dccff31b71bd0
unknown <Christian@.(none)>
2009-11-17 00:29:56 +0100

    PairSwitch redesigned

--- src/logic/searches/neighborhoods/PairSwitch.java
+++ src/logic/searches/neighborhoods/PairSwitch.java
@@ -15,43 +15,72 @@ import java.util.ArrayList;
  */
 public class PairSwitch implements INeighborhood {
 
-    private ArrayList<Solution> neighborhood;
+    private ArrayList<Job> jobsequence;
     private int i;
     private int j;
+    private int n;
 
     public PairSwitch() {
-        neighborhood = new ArrayList<Solution>();
-
+        super();
     }
 
-    public ArrayList<Solution> getNeighborhood(){
-        return this.neighborhood;
+    public ArrayList<Solution> getNeighborhood(Solution solution){
+        ArrayList<Solution> neighborhood = new ArrayList<Solution>();
+        ArrayList<Job> jobseq = solution.jobs;
+        Job lJob;
+        Job kJob;
+
+        for (int k = 0; k < jobseq.size(); k++) {
+            for (int l = k; l < jobseq.size(); l++) {
+                if(k != l){
+                    ArrayList<Job> newJobSequence = new ArrayList<Job>();
+                    for (Job job : jobsequence){
+                        newJobSequence.add(job);
+                    }
+                    lJob = newJobSequence.get(l);
+                    kJob = newJobSequence.get(k);
+                    newJobSequence.set(l, kJob);
+                    newJobSequence.set(k, lJob);
+                    neighborhood.add(new Solution(newJobSequence));
+                }
+            }
+        }
+
+        return neighborhood;
     }
 
     public void init(Solution solution){
-       i = 0;
-       j = i;
-       ArrayList<Job> jobsequence = solution.jobs;
+       this.i = 0;
+       this.j = i;
+       this.jobsequence = solution.jobsequence;
+       this.n = this.jobsequence.size();
     }
-//
-//     ArrayList<Job> jobsequence = solution.jobs;
-//        Job jJob;
-//        Job iJob;
-//
-//        for (int i = 0; i < jobsequence.size(); i++) {
-//            for (int j = i; j < jobsequence.size(); j++) {
-//                if(i != j){
-//                    ArrayList<Job> newJobSequence = jobsequence;
-//                    jJob = newJobSequence.get(j);
-//                    iJob = newJobSequence.get(i);
-//                    newJobSequence.set(j, iJob);
-//                    newJobSequence.set(i, jJob);
-//                    neighborhood.add(new Solution(newJobSequence));
-//                }
-//            }
-//        }
+
+
 
     public Solution next() {
-        return null;
+        Job iJob;
+        Job jJob;
+
+        if (!((i == (n-1)) && (j == n))){
+            ArrayList<Job> newJobSequence = new ArrayList<Job>();
+            for (Job job : jobsequence){
+                newJobSequence.add(job);
+            }
+
+            jJob = newJobSequence.get(j);
+            iJob = newJobSequence.get(i);
+            newJobSequence.set(j, iJob);
+            newJobSequence.set(i, jJob);
+            if(j == (n-1)){
+                i++;
+                j = 0;
+            }else{
+                j++;
+            }
+             return new Solution(newJobSequence);
+        }else{
+            return null;
+        }
     }
 }
