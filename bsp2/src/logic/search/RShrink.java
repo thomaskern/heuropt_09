954068724512923d026efeb8a09d2294f13f0fcf
unknown <Christian@.(none)>
2009-12-29 01:28:36 +0100

    foster parents added to rshrink

--- bsp2/src/logic/search/RShrink.java
+++ bsp2/src/logic/search/RShrink.java
@@ -22,20 +22,10 @@ public class RShrink implements INeighborhood {
     }
 
     public TrieNodeList getFosterParents(Trie best, TrieNode node){
-        Iterator<TrieNode> it = best.getTreeNodes().iterator();
-        TrieNode tmp = null;
-        TrieNodeList nd = getNonDescendants(best,node);
-        TrieNode pa = node.getParent();
-        TrieNodeList fpa = new TrieNodeList();
-
-        while(it.hasNext()){
-            tmp = it.next();
-            if(!(pa.equals(tmp) || nd.contains(tmp))){
-                fpa.add(tmp);
-            }
-        }
-
-        return fpa;
+       Iterator<TrieNode> it = best.getTreeNodes().iterator();
+       TrieNodeList fpa = getNonDescendants(best,node);
+       fpa.remove(node.getParent());
+       return fpa;
     }
 
     public TrieNodeList getNonDescendants(Trie best, TrieNode node) {
